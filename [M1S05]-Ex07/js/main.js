bcadbedca18915281b815d6954c3dae06867a395
Lucas <lucaswillyaraujo@gmail.com>
2024-03-08 18:05:07 -0300

    [M1S05]-Ex07

---
+++ [M1S05]-Ex07/js/main.js
@@ -0,0 +1,69 @@
+const inputProduto = document.getElementById("input-produto");
+
+const respostaH4 = document.getElementById("resposta-preco");
+
+var morango = {nome: "Morango", valor: 1.99}
+var banana = {nome: "Banana", valor: 2.99 };
+var abacaxi = {nome: "Abacaxi", valor: 3.99 };
+var mamao = {nome: "Mamao", valor: 4.99 };
+
+
+function consultarPreco() {
+  const cod = inputProduto.value;
+  const resposta = precos(cod);
+  respostaH4.innerText = `Preço: ${resposta.nome}: R$${resposta.valor}`;
+}
+
+function precos(cod) {
+  var cod = document.getElementById("input-produto").value;
+
+  switch (cod) {
+    case "1":
+      return (morango);
+    case "2":
+      return (banana);
+    case "3":
+      return (abacaxi);
+    case "4":
+      return (mamao);
+    default:
+      alert('Cógido não encontrado. Digite um código válido.')
+  }
+}
+
+
+var carrinho = [];
+
+function addCarrinho() {
+  const cod = inputProduto.value;
+  const produto = precos(cod);
+
+  if (produto) {
+    carrinho.push(produto);
+    alert(`${produto.nome} adicionado ao carrinho.`);
+    atualizaCarrinho();
+
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  }
+}
+
+function atualizaCarrinho() {
+  let total = 0;
+  let qtdItens = 0;
+  carrinho.forEach((item) => {
+    total += parseFloat(item.valor);
+    qtdItens += 1;
+  });
+  const totalCarrinhoElement = document.getElementById("itensCarrinho");
+  totalCarrinhoElement.textContent = `Total do carrinho: R$${total.toFixed(2)}`;
+  const totalItens = document.getElementById('qtd-itens');
+  totalItens.textContent = `Quantidade de itens: ${qtdItens}`;
+}
+
+window.onload = function () {
+  if (localStorage.getItem("carrinho")) {
+    carrinho = JSON.parse(localStorage.getItem("carrinho"));
+    atualizaCarrinho();
+    atualizaNumeroItensCarrinho();
+  }
+};
